edc3d008e945629a8100afd22cd2e662e6b7352c
leannethng <leanne.thng@gmail.com>
2015-08-12 18:21:00 -0700

    initial commit of JSON testing project
    Using a Hearthstone stat tracker to play with AJAX and JSON

---
+++ js/widget.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $.getJSON('../data/classes.json', function (response) {
+    var statusHTML = '<ul class="bulleted">';
+    $.each(response,function (index, hero) {
+      console.log(typeof hero);
+      if (hero.result === "win") {
+        statusHTML +='<li class="won">';
+      } else {
+        statusHTML +='<li class="lost">';
+      }
+      statusHTML += hero.hero + '</li>';
+    });
+    statusHTML += '</ul>';
+    $('#games').html(statusHTML);
+  }); // end getJSON
+
+  //   //rooms
+  //   $.getJSON('../data/rooms.json', function (data) {
+  //   var roomHTML = '<ul class="rooms">';
+  //   $.each(data,function (index, room) {
+  //     if (room.available === true) {
+  //       roomHTML +='<li class="empty">';
+  //     } else {
+  //       roomHTML +='<li class="full">';
+  //     }
+  //     roomHTML += room.room + '</li>';
+  //   });
+  //   roomHTML += '</ul>';
+  //   $('#roomList').html(roomHTML);
+  //
+  // }); // end getJSON
+
+}); // end ready
