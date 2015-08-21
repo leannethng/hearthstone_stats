9abafe9a5e4381d57bcb91c8d2317ba7cdd7d418
leannethng <leanne.thng@gmail.com>
2015-08-20 21:04:12 -0700

    added in a second set of data
    used existing code to test out how to select the winloss data in the
    Ajax request. stuck on how to pull out the name of each hero

--- js/widget.js
+++ js/widget.js
@@ -13,21 +13,24 @@ $(document).ready(function () {
     statusHTML += '</ul>';
     $('#games').html(statusHTML);
   }); // end getJSON
-}); // end ready
 
-// $(document).ready(function () {
-//   $.getJSON('../data/winLoss.json', function (response) {
-//     var statusHTML = '<ul class="bulleted">';
-//     $.each(response.history,function (index, hero) {
-//       console.log(typeof hero);
-//       if (hero.result === "win") {
-//         statusHTML +='<li class="won">';
-//       } else {
-//         statusHTML +='<li class="lost">';
-//       }
-//       statusHTML += hero.hero + ' vs ' + hero.opponent + '</li>';
-//     });
-//     statusHTML += '</ul>';
-//     $('#games').html(statusHTML);
-//   }); // end getJSON
-// }); // end ready
+
+    $.getJSON('../data/winLoss.json', function (response) {
+      var statusHTML = '<ul class="bulleted">';
+      $.each(response.stats.as_class,function (index, data) {
+        console.log(typeof data);
+        if (data.wins > 0) {
+          statusHTML +='<li class="won">';
+        } else {
+          statusHTML +='<li class="lost">';
+        }
+        statusHTML += data.total +" "+ data.as_class + '</li>';
+      });
+      statusHTML += '</ul>';
+      $('#winloss').html(statusHTML);
+    }); // end getJSON
+
+
+
+
+}); // end ready
