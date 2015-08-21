e9fd021bddcf4554d483c534ae80b251e1bd2ad2
leannethng <leanne.thng@gmail.com>
2015-08-20 20:28:28 -0700

    added a css bar chart to page
    going to try to make this in jQuery to populate the html

--- js/widget.js
+++ js/widget.js
@@ -14,3 +14,20 @@ $(document).ready(function () {
     $('#games').html(statusHTML);
   }); // end getJSON
 }); // end ready
+
+// $(document).ready(function () {
+//   $.getJSON('../data/winLoss.json', function (response) {
+//     var statusHTML = '<ul class="bulleted">';
+//     $.each(response.history,function (index, hero) {
+//       console.log(typeof hero);
+//       if (hero.result === "win") {
+//         statusHTML +='<li class="won">';
+//       } else {
+//         statusHTML +='<li class="lost">';
+//       }
+//       statusHTML += hero.hero + ' vs ' + hero.opponent + '</li>';
+//     });
+//     statusHTML += '</ul>';
+//     $('#games').html(statusHTML);
+//   }); // end getJSON
+// }); // end ready
