4559e3ee1389ab1b99bbde814424bc813d806bd1
leannethng <leanne.thng@gmail.com>
2015-08-21 17:02:32 -0700

    got the jQuery populating a table
    decided to maybe try and now make a flex box display not a table as
    table column positioning is getting pretty messy and could be done
    better with another method

--- js/widget.js
+++ js/widget.js
@@ -1,12 +1,12 @@
-$(document).ready(function () {
-  $.getJSON('../data/classes.json', function (response) {
+$(document).ready(function() {
+  $.getJSON('../data/classes.json', function(response) {
     var statusHTML = '<ul class="bulleted">';
-    $.each(response.history,function (index, hero) {
+    $.each(response.history, function(index, hero) {
       // console.log(typeof hero);
       if (hero.result === "win") {
-        statusHTML +='<li class="won">';
+        statusHTML += '<li class="won">';
       } else {
-        statusHTML +='<li class="lost">';
+        statusHTML += '<li class="lost">';
       }
       statusHTML += hero.hero + ' vs ' + hero.opponent + '</li>';
     });
@@ -15,28 +15,52 @@ $(document).ready(function () {
   }); // end getJSON
 
 
-    $.getJSON('../data/winLoss.json', function (response) {
-      var statusHTML = '<ul class="bulleted">';
-      $.each(response.stats.as_class,function (index, data) {
-        // console.log(typeof data);
-        if (data.wins > 0) {
-          statusHTML +='<li class="won">';
-        } else {
-          statusHTML +='<li class="lost">';
-        }
+  // $.getJSON('../data/winLoss.json', function(response) {
+  //   var statusHTML = '<ul class="bulleted">';
+  //   $.each(response.stats.as_class, function(index, data) {
+  //     // console.log(typeof data);
+  //     if (data.wins > 0) {
+  //       statusHTML += '<li class="won">';
+  //     } else {
+  //       statusHTML += '<li class="lost">';
+  //     }
+  //
+  //
+  //     statusHTML += data.total;
+  //
+  //     $.each(response.stats.as_class, function(key, value) {
+  //       console.log(value);
+  //       statusHTML += " " + key + '</li>';
+  //     });
+  //
+  //   });
+  //   statusHTML += '</ul>';
+  //   $('#winloss').html(statusHTML);
+  // }); // end getJSON
 
-
-        statusHTML += data.total;
-
-        $.each(response.stats.as_class,function (key, value) {
-            console.log(value);
-        statusHTML += " " + key + '</li>';
-        });
-
-      });
-      statusHTML += '</ul>';
-      $('#winloss').html(statusHTML);
-    }); // end getJSON
+  // $.getJSON('../data/winLoss.json', function(response) {
+  //   var statusHTML = '<tbody>';
+  //   $.each(response.stats.as_class, function(key, value) {
+  //     statusHTML += "<tr class=" + key + " " +"id=" + key +">";
+  //
+  //
+  //     statusHTML += "<th scope='row'>"+ key +"</th>";
+  //       statusHTML += "<td class='loss bar' style='height: 24px;'>";
+  //       statusHTML += "</td>";
+  //       statusHTML += "<td class='win bar' style='height: 24px;'>";
+  //       statusHTML += "</td>";
+  //     statusHTML += "</tr>";
+  //
+  //     // $.each(response.stats.as_class, function(key, value) {
+  //     //   console.log(value);
+  //     //
+  //     // });
+  //
+  //   });
+  //   statusHTML += '</tbody>';
+  //   $('#table').html(statusHTML);
+  //   $("#table").appendTo($("#graph"))
+  // }); // end getJSON
 
 
 
