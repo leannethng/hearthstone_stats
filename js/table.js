9fadc00b8e5c297f6ea2d2dac3f602a48b2feb10
leannethng <leanne.thng@gmail.com>
2015-08-23 19:05:35 -0700

    adding hover jquery
    messed around with this for a bit but not quite working yet.

---
+++ js/table.js
@@ -0,0 +1,10 @@
+// $(function() {
+//   $(document).on('mousehover','.win-key', function() {
+//     if($('.win').hasClass('w')) {
+//        $('.win').toggleClass('win:hover');
+//     }
+//     if($('.loss').hasClass('l')) {
+//        $('.loss').toggleClass('loss:hover');
+//     }
+//   });
+// });
