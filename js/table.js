be04907982083dd448ea5e2fb97c07221343b3b2
leannethng <leanne.thng@gmail.com>
2015-08-28 11:48:40 -0700

    added fresh data and got the sorting to work in Safari
    added in a conditional operator and it now works in Safari

--- js/table.js
+++ js/table.js
@@ -3,17 +3,17 @@ $(document).ready(function() {
 //Sorting table
 $('button.key.loss-key').on('click', function () {
     var numericallyOrderedDivs = $(".columns").sort(function (a, b) {
-        return parseInt($(a).find("div.win.bar").children("p").text()) >parseInt($(b).find("div.win.bar").children("p").text());
+        return parseInt($(a).find("div.win.bar").children("p").text(),10) > parseInt($(b).find("div.win.bar").children("p").text(),10) ? 1 : -1;
     });
     $("#graph").html(numericallyOrderedDivs);
-      console.log(numericallyOrderedDivs)
+      console.log(numericallyOrderedDivs);
 });
 
 $('button.key.win-key').on('click', function () {
     var numericallyOrderedDivs = $(".columns").sort(function (a, b) {
-        return parseInt($(a).find("div.loss.bar").children("p").text()) > parseInt($(b).find("div.loss.bar").children("p").text());
+        return parseInt($(a).find("div.loss.bar").children("p").text(),10) > parseInt($(b).find("div.loss.bar").children("p").text(),10)? 1 : -1;
     });
     $("#graph").html(numericallyOrderedDivs);
-
 });
+
 });
