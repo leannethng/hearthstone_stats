d27eae5598f2fd654a5ec76b67d619a0b189dec9
leannethng <leanne.thng@gmail.com>
2015-09-04 23:00:35 -0700

    starting to add own data to percentage
    doesn’t work yet need to figure out the code

--- js/avg_percentage.js
+++ js/avg_percentage.js
@@ -1,16 +1,29 @@
-var myCircle = Circles.create({
-  id:                  'circles-1',
-  radius:              70,
-  value:               43,
-  maxValue:            100,
-  width:               10,
-  text:                function(value){return value + '%';},
-  colors:              ['#D3B6C6', '#4B253A'],
-  duration:            400,
-  wrpClass:            'circles-wrp',
-  textClass:           'circles-text',
-  valueStrokeClass:    'circles-valueStroke',
-  maxValueStrokeClass: 'circles-maxValueStroke',
-  styleWrapper:        true,
-  styleText:           true
+$(document).ready(function() {
+  $.getJSON('http://www.leannethng.com/hearthstone_stats/data/winLoss.json', function(response) {
+
+  $.each(response.stats, function(key, value) {
+      var winrate =(value.wins / value.total) * 100;
+      console.log(winrate.toFixed(0));
+
+
+
+  var myCircle = Circles.create({
+
+    id:                  'circles-1',
+    radius:              70,
+    value:               function(winrate){return winrate;},
+    maxValue:            100,
+    width:               10,
+    text:                function(value){return value + '%';},
+    colors:              ['#D3B6C6', '#4B253A'],
+    duration:            400,
+    wrpClass:            'circles-wrp',
+    textClass:           'circles-text',
+    valueStrokeClass:    'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper:        true,
+    styleText:           true
+  });
+  })
+});
 });
